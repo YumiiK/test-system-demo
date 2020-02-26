f32bb8e2ddd76ad4cb0bd662d91e26feb497ad8e
TiliaceaeSU <48793115+TiliaceaeSU@users.noreply.github.com>
2020-02-26 20:15:24 +0800

    Update surveys.py

--- surveys.py
+++ surveys.py
@@ -1,7 +1,9 @@
+import json
+
 class Question:
     """Question on a questionnaire."""
 
-    def __init__(self, question, choices=None, allow_text=False):
+    def __init__(self, question, choices=None,answer=None, image=None,allow_text=False):
         """Create question (assume Yes/No for choices."""
 
         if not choices:
@@ -9,7 +11,10 @@ class Question:
 
         self.question = question
         self.choices = choices
+        self.answer = answer
+        self.image = image
         self.allow_text = allow_text
+        
 
 
 class Survey:
@@ -23,3 +28,39 @@ class Survey:
         self.questions = questions
 
 
+
+                    
+                    
+# trial = Survey(
+#     "Trial",
+#     "In this section, we have prepared several questions to familiarize you with them. After you choose the "
+#     "answer, the correct answer will appear.",
+#     [
+#         Question("Have you shopped here before?"),
+#         Question("Did someone else shop with you today?"),
+#         Question("On average, how much do you spend a month on frisbees?",
+#                  ["Less than $10,000", "$10,000 or more"]),
+#         Question("Are you likely to shop here again?"),
+#     ])
+
+# test = Survey(
+#     "Test",
+#     "In this section, you have 45 questions to answer. After every 15 questions, you can take a rest, then continue. "
+#     "Begin the formal test now!",
+#     [
+#         Question("Do you ever dream about code?"),
+#         Question("Do you ever have nightmares about code?"),
+#         Question("Do you prefer porcupines or hedgehogs?",
+#                  ["Porcupines", "Hedgehogs"]),
+#         Question("Which is the worst function name, and why?",
+#                  ["do_stuff()", "run_me()", "wtf()"],
+#                  allow_text=True),
+#     ]
+# )
+
+
+
+# surveys = {
+#     "trial": trial,
+#     "test": test,
+# }
